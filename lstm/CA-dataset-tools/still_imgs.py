c4f4614ba6f619a5cadffe483dd908332a72170f
Matt Saunders <msaund05@mail.uoguelph.ca>
2017-05-16 16:04:10 -0400

    Created a tool to crop, label, and pickle the still images for the first training part.

--- lstm/CA-dataset-tools/still_imgs.py
+++ lstm/CA-dataset-tools/still_imgs.py
@@ -1,8 +1,13 @@
 import os
 import cPickle
+import numpy as np 
+import scipy as scp
+from scipy import ndimage
+from tqdm import tqdm
 
-def organize_training_data(path_to_dataset):
-    #Get list of directories within dataset path.
+PATH='/mnt/data/datasets/collective-activity'
+
+def get_dir_list(path_to_dataset):
     files = os.listdir(path_to_dataset)
     dirs = []
     for entry in files:
@@ -10,8 +15,13 @@ def organize_training_data(path_to_dataset):
             dirs.append(entry)
 
     dirs.sort()
+    return dirs
+
+def organize_training_data(path_to_dataset):
+    #Get list of directories within dataset path.
+    dirs = get_dir_list(path_to_dataset)
     print(dirs)
-    #Iterate through each file in each directory, assigning each path an integer key in a dict.
+    #Iterate through each file in each directory, appending each file's path to a list
     path_list = []
     for entry in dirs:
         cur_path = path_to_dataset + '/' + entry
@@ -25,8 +35,48 @@ def organize_training_data(path_to_dataset):
     f = open(path_to_dataset + '/' + 'image_list.dat', 'wb')
     cPickle.dump(path_list, f)
 
-def main(unused_argv):
-    organize_training_data('/mnt/data/datasets/collective-activity')
+def crop_still_imgs(path_to_dataset):
+    output_path = path_to_dataset + '/stills/'
+    dirs = get_dir_list(path_to_dataset+'/raw')
+    # For each directory:
+    imgs = []
+    labels = []
+    poses = []
 
+    for d in dirs:
+        cur_dir = path_to_dataset + '/raw/' + d
+        with open(cur_dir+'/annotations.txt') as fo:
+            print(d)
+            for line in tqdm(fo):
+                strings = line.split()
+
+                if len(strings[0]) == 3:
+                    num = '0'+strings[0]
+                else:
+                    num = strings[0]
+                
+                image_path = cur_dir+'/frame'+num+'.jpg'
+                tlc = (int(strings[1]), int(strings[2]))
+                size = (int(strings[3]), int(strings[4]))
+                label = int(strings[5])
+                pose = int(strings[6])
+
+                with open(image_path) as img:
+                    array = scp.misc.imread(img)
+                    crop = array[tlc[1]:tlc[1]+size[1], tlc[0]:tlc[0]+size[0]]
+                imgs.append(crop)
+                labels.append(label)
+                poses.append(pose)
+    
+    data = {'images': imgs,
+            'labels': labels,
+            'poses': poses}
+
+    f = open(output_path + 'cropped')
+    cPickle.dump(data, f)
+
+def main(unused_argv):
+    crop_still_imgs(PATH)
+    
 if __name__ == '__main__':
     main(0)
