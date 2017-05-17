979bd6d1fb31669fecd440995f466e51fd1c521f
Matt Saunders <msaund05@mail.uoguelph.ca>
2017-05-17 17:20:41 -0400

    Working on dataset massaging.

--- lstm/CA-dataset-tools/still_imgs.py
+++ lstm/CA-dataset-tools/still_imgs.py
@@ -1,11 +1,20 @@
+from __future__ import division
+
 import os
 import cPickle
 import numpy as np 
 import scipy as scp
 from scipy import ndimage
 from tqdm import tqdm
+import PIL
+from PIL import Image
 
 PATH='/mnt/data/datasets/collective-activity'
+
+def unpickle(file_name):
+    with open(file_name, 'rb') as fo:
+        data = cPickle.load(fo)
+    return data
 
 def get_dir_list(path_to_dataset):
     files = os.listdir(path_to_dataset)
@@ -48,7 +57,7 @@ def crop_still_imgs(path_to_dataset):
         cur_dir = path_to_dataset + '/raw/' + d
         with open(cur_dir+'/annotations.txt') as fo:
             print(d)
-            for line in tqdm(fo):
+            for idx, line in tqdm(enumerate(fo)):
                 strings = line.split()
 
                 if len(strings[0]) == 3:
@@ -64,7 +73,16 @@ def crop_still_imgs(path_to_dataset):
 
                 with open(image_path) as img:
                     array = scp.misc.imread(img)
+                    if tlc[1] < 0:
+                        tlc = (0, tlc[0])
+                    if tlc[0] < 0:
+                        tlc = (tlc[1], 0)
+
                     crop = array[tlc[1]:tlc[1]+size[1], tlc[0]:tlc[0]+size[0]]
+                    if crop.shape[1] == 0:
+                        print(d),
+                        print(': %d: zero width: %s' %(idx, line))
+
                 imgs.append(crop)
                 labels.append(label)
                 poses.append(pose)
@@ -77,8 +95,97 @@ def crop_still_imgs(path_to_dataset):
     cPickle.dump(data, f)
     f.close()
 
+def analyze_dataset(path_to_dataset):
+    data = unpickle(path_to_dataset)
+    widths = {}
+    heights = {}
+    ratios = {}
+
+    for idx, img in enumerate(data['images']):
+        height = img.shape[0]
+        width = img.shape[1]
+        try:
+            ratio = height / width
+            ratio = int(ratio * 10)
+        except ZeroDivisionError:
+            print("Divide by zero in image %d. Shape: " %idx), 
+            print(img.shape)
+            ratio = 9999
+
+        if height in heights:
+            heights[height] += 1
+        else:
+            heights[height] = 1
+
+        if width in widths:
+            widths[width] += 1
+        else:
+            widths[width] = 1
+
+        if ratio in ratios:
+            ratios[ratio] += 1
+        else:
+            ratios[ratio] = 1
+
+    heightkeys = []
+    heightvalues = []
+    for key, value in heights.items():
+        heightkeys.append(key)
+        heightvalues.append(value)
+    heightkeys, heightvalues = zip(*sorted(zip(heightkeys, heightvalues)))
+    for i in range(len(heightkeys)):
+        print("%02d: %d" %(heightkeys[i], heightvalues[i]))
+
+    widthkeys = []
+    widthvalues = []
+    for key, value in widths.items():
+        widthkeys.append(key)
+        widthvalues.append(value)
+    widthkeys, widthvalues = zip(*sorted(zip(widthkeys, widthvalues)))
+    for i in range(len(widthkeys)):
+        print("%02d: %d" %(widthkeys[i], widthvalues[i]))
+
+    ratiokeys = []
+    ratiovalues = []
+    for key, value in ratios.items():
+        ratiokeys.append(key)
+        ratiovalues.append(value)
+    ratiokeys, ratiovalues = zip(*sorted(zip(ratiokeys, ratiovalues)))
+    for i in range(len(ratiokeys)):
+        print("%02d: %d" %(ratiokeys[i], ratiovalues[i]))
+
+    h_idx = idx_max(heightvalues)
+    w_idx = idx_max(widthvalues)
+
+    max_h = heightkeys[h_idx]
+    max_w = widthkeys[w_idx]
+
+    print('mode of heights, widths: %d, %d' %(max_h, max_w))
+
+def idx_max(values):
+    return values.index(max(values))
+
+def resize_cropped_imgs(path_to_dataset):
+    target_height = 260
+    target_width = 130
+    data = unpickle(path_to_dataset+'cropped')
+    img_res = []
+    for img in tqdm(data['images']):
+        pil_img = scp.misc.toimage(img)
+        img_res.append(np.array(pil_img.resize((target_width, target_height), PIL.Image.ANTIALIAS)))
+
+    new_data = {'images': img_res,
+                'labels': data['labels'],
+                'poses': data['poses']}
+
+    f = open(path_to_dataset + 'resized', 'wb')
+    cPickle.dump(new_data, f)
+    f.close()
+
 def main(unused_argv):
-    crop_still_imgs(PATH)
+    #crop_still_imgs(PATH)
+    resize_cropped_imgs(PATH+'/stills/')
+    #analyze_dataset(PATH+'/stills/cropped')
     
 if __name__ == '__main__':
     main(0)
